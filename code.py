5cfd32e56322fb647a0fd4bfeb5752492bb18035
SirP99 <75451051+SirP99@users.noreply.github.com>
2024-04-14 21:17:30 +0200

    idorendiseg

--- code.py
+++ code.py
@@ -39,10 +39,10 @@ for idx, item in enumerate(items):
     st.write(f'Tárgy {idx + 1}')
     item['name'] = st.selectbox(f'Válassz egy tárgyat', list(items_data.keys()), index=0 if item['name'] == '' else list(items_data.keys()).index(item['name']), key=f'item_name_{idx}')
     item['quantity_or_hours'] = st.number_input(f'Mennyiség vagy óraszám', min_value=1, value=item['quantity_or_hours'], key=f'quantity_or_hours_{idx}')
-    if st.button(f'Törlés {idx + 1}', key=f'delete_button_{idx}'):
+    if st.checkbox(f'Törlés {idx + 1}', key=f'delete_checkbox_{idx}'):
         deleted_items.append(idx)
 
-if st.button('Hozzáadás'):
+if st.checkbox('Hozzáadás'):
     items.append({'name': 'Mosogatás', 'quantity_or_hours': 1})
 
 for idx in sorted(deleted_items, reverse=True):
