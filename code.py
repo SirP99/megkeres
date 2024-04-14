6eeec8940c4668e78abf81c9490a625a510e8142
SirP99 <75451051+SirP99@users.noreply.github.com>
2024-04-14 20:35:32 +0200

    nagy alakitas

--- code.py
+++ code.py
@@ -22,25 +22,36 @@ interest = st.text_input('Érdeklődés tárgya', key='interest_input')
 # Rendelés tételek
 items = []
 counter = 0
-while st.button(f'Tétel hozzáadása {counter + 1}', key=f'add_button_{counter}'):
+while st.button(f'Új tétel hozzáadása {counter + 1}', key=f'add_button_{counter}'):
     item_name = st.text_input('Tétel neve', key=f'item_name_{counter}')
-    quantity = st.number_input('Mennyiség', min_value=1, value=1, key=f'quantity_{counter}')
-    hours = st.number_input('Óraszám', min_value=1, value=1, key=f'hours_{counter}')
+    quantity_or_hours = st.number_input('Mennyiség vagy óraszám', min_value=1, value=1, key=f'quantity_or_hours_{counter}')
+    unit_type = st.selectbox('Mérték', ['Darab', 'Óra'], key=f'unit_type_{counter}')
     
     # Elrejtjük az egységárat és használjuk a programban meghatározott értéket
     item_price = 1000  # Állítsd be az árat, amit szeretnél
     
-    # Hozzáadás gomb
     if st.button(f'Hozzáadás {counter + 1}', key=f'add_item_{counter}'):
         items.append({
             'name': item_name,
-            'quantity': quantity,
-            'hours': hours,
-            'price': calculate_price(item_price, quantity, hours)
+            'quantity_or_hours': quantity_or_hours,
+            'unit_type': unit_type,
+            'price': calculate_price(item_price, quantity_or_hours) if unit_type == 'Darab' else calculate_price(item_price, hours=quantity_or_hours)
         })
         counter += 1
 
-    st.write(f'Egységár: {item_price} Ft')
+# Tétel törlése
+if st.button('Tétel törlése'):
+    del items[-1]
+
+# Tételek listája
+st.markdown('### Tételek')
+for idx, item in enumerate(items):
+    template = f"""
+    **{item['name']}**
+    Mennyiség/Óraszám: {item['quantity_or_hours']} {item['unit_type']}
+    Ár: {item['price']} Ft
+    """
+    st.write(template)
 
 # Ajánlat generálása
 st.markdown(f'### Ajánlat')
@@ -63,8 +74,7 @@ Cím: {address}
 for idx, item in enumerate(items):
     template += f"""
 Ajánlati tétel {idx + 1}: {item['name']}
-Mennyiség: {item['quantity']} db
-Óraszám: {item['hours']} óra
+Mennyiség/Óraszám: {item['quantity_or_hours']} {item['unit_type']}
 Összár: {item['price']} Ft
 """
 
